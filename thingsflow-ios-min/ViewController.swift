0e8c9fb4a62740b8a91376f5ef9fa58a81f7c791
minkyungjun <q_alz@naver.com>
2022-11-25 12:32:12 +0900

    project 생성

---
+++ thingsflow-ios-min/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  thingsflow-ios-min
+//
+//  Created by 민경준 on 2022/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
